d6add142bffc2c053b92dd14133c8141d6481c84
thiago william <thiago.will.silva@gmail.com>
2020-04-02 19:14:31 +0100

    Updates

---
+++ precache-manifest.e5d991e8ac4db98fb70720f718349107.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161601ae1324ff737dabca2cd58e9283",
+    "url": "/api-weather/index.html"
+  },
+  {
+    "revision": "775d99ce62b9cd3ce94a",
+    "url": "/api-weather/static/js/2.7e345583.chunk.js"
+  },
+  {
+    "revision": "115c29c453b61995b5be0af8c45bd83e",
+    "url": "/api-weather/static/js/2.7e345583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94e68fae1cd1061b05f",
+    "url": "/api-weather/static/js/main.cda8b20a.chunk.js"
+  },
+  {
+    "revision": "62b706cfb23f5416352b",
+    "url": "/api-weather/static/js/runtime-main.14f66b85.js"
+  },
+  {
+    "revision": "35a6dc544ace616135d4d1c7fa441cd9",
+    "url": "/api-weather/static/media/logo.35a6dc54.svg"
+  }
+]);
